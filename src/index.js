3ae82590af531f289d8888f54685906b03291a4c
Wangwange <ltk6031@naver.com>
2020-04-17 17:09:39 +0900

    #~27

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "antd/dist/antd.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./_reducers";
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    promiseMiddleware,
+    ReduxThunk,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
